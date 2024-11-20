d7045338608266195a69c91db9c500de28733fa7
alessandro <george.pantazi@iisgalvanimi.edu.it>
2024-11-20 10:46:49 +0100

    punto 2

--- progettino/listaSpesa.py
+++ progettino/listaSpesa.py
@@ -1,1 +1,8 @@
 lista_spesa = []
+
+#2
+def aggiungi_elemento():
+    elemento = input("Inserisci l'elemento da aggiungere alla lista: ")
+    lista_della_spesa.append(elemento)
+    print(f"{elemento} è stato aggiunto alla lista!")
+    
