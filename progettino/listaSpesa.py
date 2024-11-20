ceaa8e3860e20157045fec69488b78d09ecba2d4
alessandro <george.pantazi@iisgalvanimi.edu.it>
2024-11-20 10:49:16 +0100

    punto 3

--- progettino/listaSpesa.py
+++ progettino/listaSpesa.py
@@ -1,8 +1,17 @@
-lista_spesa = []
+lista_della_spesa = []
 
 #2
 def aggiungi_elemento():
     elemento = input("Inserisci l'elemento da aggiungere alla lista: ")
     lista_della_spesa.append(elemento)
     print(f"{elemento} è stato aggiunto alla lista!")
-    
+
+
+#3
+def visualizza_lista():
+    if lista_della_spesa:
+        print("\nLista della spesa:")
+        for i, elemento in enumerate(lista_della_spesa):
+            print(f"{i}. {elemento}")
+    else:
+        print("La lista della spesa è vuota.")
